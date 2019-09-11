11a42a70a602fecdb7138b755f2e220626618311
Oeivind Hagenes <OHagenes2@slb.com>
2019-09-11 15:43:09 +0200

    Exit button added

--- MyFirstGitProject/Form1.cs
+++ MyFirstGitProject/Form1.cs
@@ -16,5 +16,10 @@ namespace MyFirstGitProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
